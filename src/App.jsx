9583fa0122fc7cfc4d644f78ccb855483b4e9967
Lâm Trường <lamtruongpk31@gmail.com>
2022-10-27 14:56:56 +0700

    add library and config router, layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-import { useState } from 'react'
-import './App.css'
+import { Fragment, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <div className="App">
-      <h1>Welcome to project my tiktok</h1>
-    </div>
-  )
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        let Layout = DefaultLayout;
+                        if (route.layout) Layout = route.layout;
+                        else if (route.layout === null) Layout = Fragment;
+                        const Page = route.component;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
-export default App
+export default App;
